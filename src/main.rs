b254442be8799a75e9a75fe96d03647cf63554fd
itt828 <iittt828@gmail.com>
2023-06-14 23:21:27 +0900

    :sparkles: First page

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use yew::prelude::*;
+
+struct App {
+    value: i64,
+}
+
+enum Msg {
+    AddOne,
+}
+
+impl Component for App {
+    type Message = Msg;
+    type Properties = ();
+    fn create(ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            }
+        }
+    }
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div>
+                <div>
+                    <button onclick={ctx.link().callback(|_|Msg::AddOne)}>{"+1"}</button>
+                </div>
+                <div>
+                    {"おいすー"}
+                    {format!("カウント: {}",self.value)}
+                </div>
+            </div>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
